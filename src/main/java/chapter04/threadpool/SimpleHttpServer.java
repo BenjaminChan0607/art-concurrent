95f47e1c26bb72adb5cceff97b793eccbee5b84b
chend <Mf@20171018>
2018-08-08 19:24:51 +0800

    mod

---
+++ src/main/java/chapter04/threadpool/SimpleHttpServer.java
@@ -0,0 +1,105 @@
+package chapter04.threadpool;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by benjaminChan on 2018/8/8 0008 下午 7:13.
+ */
+public class SimpleHttpServer {
+
+
+    static ThreadPool<HttpRequestHandler> threadPool = new DefaultThreadPool<HttpRequestHandler>(11);
+    static String basePath = "D:\\workspace\\extraProject\\art-concurrent\\src\\main\\java\\chapter04\\threadpool";
+    static ServerSocket serverSocket;
+    static int port = 8080;
+
+    public static void setPort(int port) {
+        if (port > 0) {
+            SimpleHttpServer.port = port;
+        }
+    }
+
+    public static void setBasePath(String basePath) {
+        if (basePath != null && new File(basePath).exists() && new File(basePath).isDirectory()) {
+            SimpleHttpServer.basePath = basePath;
+        }
+    }
+
+    public static void start() throws IOException {
+        serverSocket = new ServerSocket(port);
+        Socket socket;
+        while ((socket = serverSocket.accept()) != null) {
+            threadPool.execute(new HttpRequestHandler(socket));
+        }
+        serverSocket.close();
+    }
+
+    static class HttpRequestHandler implements Runnable {
+
+        private Socket socket;
+
+        public HttpRequestHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            String line = null;
+            BufferedReader br = null;
+            BufferedReader reader = null;
+            PrintWriter out = null;
+            InputStream in = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String header = reader.readLine();
+                String filePath = basePath + header.split(" ")[1];
+                out = new PrintWriter(socket.getOutputStream());
+                if (filePath.endsWith("jpg") || filePath.endsWith("ico")) {
+                    in = new FileInputStream(filePath);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    int i = 0;
+                    while ((i = in.read()) != -1) {
+                        baos.write(i);
+                    }
+
+                    byte[] array = baos.toByteArray();
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Content-Type: image/jpeg");
+                    out.println("Content-Length: " + array.length);
+                    out.println("");
+                    socket.getOutputStream().write(array, 0, array.length);
+                } else {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+                    out = new PrintWriter(socket.getOutputStream());
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Content-Type: text/html; charset=UTF-8");
+                    out.println("");
+                    while ((line = br.readLine()) != null) {
+                        out.println(line);
+                    }
+                }
+                out.flush();
+            } catch (Exception ex) {
+                out.println("HTTP/1.1 500");
+                out.println("");
+                out.flush();
+            } finally {
+                close(br, in, reader, out, socket);
+            }
+        }
+
+        private void close(Closeable... closeables) {
+            if (closeables != null) {
+                for (Closeable closeable : closeables) {
+                    try {
+                        closeable.close();
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        }
+    }
+
+}
